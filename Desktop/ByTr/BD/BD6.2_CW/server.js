0f9a477f66bc45a4b58688b6db37be9f8e14f211
Kamran Khan Alwi <82154070+KAMRANKHANALWI@users.noreply.github.com>
2024-10-08 20:25:51 +0530

    Deleted directory

--- Desktop/ByTr/BD/BD6.2_CW/server.js
+++
@@ -1,5 +0,0 @@
-let { app } = require("./index");
-let PORT = 3000;
-app.listen(PORT, () => {
-  console.log("server is listening on http://localhost:", PORT);
-});
--- Desktop/ByTr/BD/BD6.4_CW/server.js
+++
@@ -1,2 +0,0 @@
-let { app } = require("./index.js");
-app.listen(3000, () => console.log("Server is running on port 3000"));
